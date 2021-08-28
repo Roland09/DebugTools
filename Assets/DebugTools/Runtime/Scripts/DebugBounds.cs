2195e93ba00fad91fc2b4428b89a9cf16958cd8d
Roland09 <roland.cidar@gmail.com>
2021-08-28 08:50:55 +0200

    fix import

--- Assets/DebugTools/Runtime/Scripts/DebugBounds.cs
+++ Assets/DebugTools/Runtime/Scripts/DebugBounds.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
-#if UNITY_EDITOR
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
 #endif
 
 namespace Rowlan.DebugTools
